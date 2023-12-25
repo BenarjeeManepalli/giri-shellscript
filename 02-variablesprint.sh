a7f7ffdb16db36f3c0dde460743083746c53fd25
Benarjee <benarjee.manepalli@gmail.com>
2023-12-25 15:17:51 +0530

    using variables print the script

---
+++ 02-variablesprint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=DAD
+PERSON2=Daughter
+echo "hello $PERSON1, How are you?"
+echo "hello $PERSON2 I am fine"
+echo "how about you my $PERSON2" 
+echo "i am fine $PERSON1 where are you"
