1026a5cdf7914a085ad03a3f486801d303ee9a15
Benarjee <benarjee.manepalli@gmail.com>
2023-12-25 15:54:10 +0530

    this is new prompting shell script file

---
+++ 05-variableswithprompt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -i USERNAME
+
+echo "please enter your User Name :: "
+
+echo "Please enter your password :: "
+
+read -i PASSWORD
+
+echo   "my user name is ${USERNAME}"
+
+echo    "my password is ${PASSWORD}"
