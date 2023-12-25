87a8831760479b253f010da5047f513a386c6c55
Benarjee <benarjee.manepalli@gmail.com>
2023-12-25 20:43:34 +0530

    this is new prompting shell script file

---
+++ 08-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VEG=("Green leafs" "brinjal" "bottle guard" "ladies finger")
+
+echo "the first vegitable is ${VEG[0]}"
+
+echo "the second vegitable is ${VEG[1]}"
+
+echo "the third vegitable is ${VEG[2]}"
+
+echo "the fourth vegitable is ${VEG[3]}"
