43ef3d052081284d3d581c572f6178049655f501
Benarjee <benarjee.manepalli@gmail.com>
2023-12-25 15:38:35 +0530

    this is new created files

--- 03-variablewithlnxcmd.sh
+++ 03-variablewithlnxcmd.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 D=$(date)
-L=$(ls-l)
+L=$(ls -l)
 C=$(cat testfile)
 
-echo ("Today date is ${D}")  #$D is denotes my date is printing.
+echo "Today date is ${D}"  #$D is denotes my date is printing.
 
-echo ("my file list is ${L}")  #$L is denotes my file list is printing.
+echo "my file list is ${L}"  #$L is denotes my file list is printing.
 
-echo ("my create file is :: ${C}") #$c is denoted my created file is printing.
-
+echo "my create file is :: ${C}" #$c is denoted my created file is printing.
