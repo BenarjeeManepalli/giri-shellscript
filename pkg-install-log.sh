81f25ffef456a76c82806ee55bf2551d197fb9be
BenarjeeManepalli <benarjee.manepalli@gmail.com>
2023-12-26 23:06:39 +0530

    pushing conditin script file in to repo

---
+++ pkg-install-log.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Package (SQL & Git) installation
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%X)
+LOG="/tmp/$0-$TIMESTAMP.log"
+
+if [ $ID -eq 0 ]
+
+then
+
+echo "You are the root user please procced installation"
+  
+else
+
+echo "ERROR:: you are not root user"
+
+exit 1
+   
+fi  # end of the condition
+
+yum install mysql -y &>>$LOG # installing the sql package
+
+if [ $? -eq 0 ]
+then 
+   echo "Installation SQL is Success"
+else
+   echo "Installation SQL is failure"
+fi # ending the condition
+
+yum install  git -y &>>$LOG # installing the git package
+
+if [ $? -eq 0 ]
+then 
+   echo "Installation Git is Success"
+else
+   echo "Installation Git is falure"
+
+fi #ending the condition
+
+yum install web -y &>>$LOG # installing the web package
+
+if [ $? -eq 0 ]
+then 
+   echo "Installation Web is Success"
+else
+   echo "Installation Web is falure"
+fi # Ending the condition.
