ba20552627d411ab41354f107fe5b714e713fafe
BenarjeeManepalli <benarjee.manepalli@gmail.com>
2023-12-26 17:55:15 +0530

    pushing conditin script file in to repo

--- functions.sh
+++ functions.sh
@@ -7,43 +7,37 @@
 ID=$(id -u)
 
 VALID(){
-    
-    if [ $? -eq 0 ]
 
+    if [ $1 -eq 0 ]
 then 
-
-   echo "Installation is Success"
-
+   echo "$2 is Success"
 else
-
-   echo "Installation is failure"
-
-fi # ending the condition
-}  # FUNCTION IS END
+   echo "ERROR :: $2 is failure"
+fi                   # ending the condition
+}                    # FUNCTION IS END
 
 if [ $ID -eq 0 ]
-
 then
 
-echo "You are the root user please procced installation"
-  
+     echo "You are the root user please procced installation"
 else
-
-echo "ERROR:: you are not root user"
-
+     echo "ERROR:: you are not root user"
 exit 1
-   
 fi  # end of the condition
 
 yum install mysql -y # installing the sql package
 
-VALID
+VALID $? "installing Sql"
 
 yum install  git -y  # installing the git package
 
-VALID
+VALID $? "instaling Git"
 
 yum install web -y # installing the web package
 
-VALID
+VALID $? "installing Web"
+
+yum install postgresql -y #installing the postgresql
+
+VALID $? "installing postgresql"
 
