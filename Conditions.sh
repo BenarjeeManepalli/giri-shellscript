fa9bdfebbeec4927b6218f343efc1250cfc0a788
BenarjeeManepalli <benarjee.manepalli@gmail.com>
2023-12-26 16:19:05 +0530

    pushing conditin script file in to repo

--- Conditions.sh
+++ Conditions.sh
@@ -2,12 +2,13 @@
 X=$1
 Y=$2
 
-echo "Please enter the values :: "
+echo "Please enter the values :: $X and $Y"
 
 if [ $X -le $Y ]
 {
-    echo "the value is not equal to given value"
+    echo "the value is not equal to given thevalue"
 
 else
     echo "the value is equal to give value"
+}
 
