56492ffcb3d39fe3f2dec7b31d980ced0570a577
BenarjeeManepalli <benarjee.manepalli@gmail.com>
2023-12-26 16:26:01 +0530

    pushing conditin script file in to repo

--- Conditions.sh
+++ Conditions.sh
@@ -6,9 +6,9 @@ echo "Please enter the values :: $X and $Y"
 
 if [ $X -le $Y ]
 then
-    echo "the value is not equal to given thevalue"
+    echo "the value is not less equal to given the value"
   else
-    echo "the value is equal to give value"
+    echo "the value is less than equal to give the value"
 fi  # fi indicate end of the condition.
 
 
