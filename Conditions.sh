713527de54c36d8089120331029b19fe7f57f1ba
BenarjeeManepalli <benarjee.manepalli@gmail.com>
2023-12-26 16:12:10 +0530

    pushing conditin script file in to repo

---
+++ Conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+X=$1
+Y=$2
+
+echo "Please enter the values :: "
+
+if [ $X -le $Y ]
+{
+    echo "the value is not equal to given value"
+
+else
+    echo "the value is equal to give value"
+
