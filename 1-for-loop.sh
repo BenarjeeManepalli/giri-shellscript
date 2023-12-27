6d6b42a8320885428c19e1a07ed37911f207a43c
BenarjeeManepalli <benarjee.manepalli@gmail.com>
2023-12-27 17:01:27 +0530

    pushing conditin script file in to repo

--- 1-for-loop.sh
+++ 1-for-loop.sh
@@ -2,10 +2,12 @@
 
 echo "the increased numbers from 1 to 100" 
 
-for no in ((i=1 i<=100 i++))
+for no in (( i=1 i<=100 i++ ))
 do
    if [ $? -eq 0 ]
    then
-   echo " the value is $no"
+        echo "the value is $no"
+   else
+        echo "the given condition is wrong"
    fi 
 done
